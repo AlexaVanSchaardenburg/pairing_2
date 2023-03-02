2867e206997cdf284c5128ac0d1f037d00e34ac4
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-03-01 17:15:57 -0700

    Add variable and use push method

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["John", "Paul", "Bob", "Ringo", "George"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var placesToVisit = ["Denver", "France", "San Diego", "Wyoming", "Kansas"]
+placesToVisit.push("Africa")
+console.log(placesToVisit)
